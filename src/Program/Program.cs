1c44a6d3be966cf49724e77ecc9f8c09f65d455e
tomasmaurente <tommaurente@gmail.com>
2021-05-02 20:50:00 -0300

    Codigo andando

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace PII_Game_Of_Life
 {
@@ -6,7 +7,28 @@ namespace PII_Game_Of_Life
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Crear la variable que contenga la url del .txt.
+            string url = "..\\board.txt";
+            // Creo un tablero
+            bool[,] tablero = CargaDeTablero.CreacionDeTablero(url); //variable que representa el tablero
+
+            // Creo un bucle que imprima La tabla inicial,
+            // luego la modifique,
+            // luego limpie la consola,
+            // y la vuelva a imprimir
+            
+            while (true)
+            {
+                // Imprime
+                Console.WriteLine(Imprimir.TableroToString(tablero));
+                // Modifica la tabla
+                tablero = Logica.Juego(tablero);
+                // Limpia la consola despues de 0.3s
+                Thread.Sleep(300);
+                Console.Clear();
+                // Vuelve a impimir
+            }
+
         }
     }
 }
